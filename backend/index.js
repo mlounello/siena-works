3b27317cbad5996c9d201cffe3bc10596eefbe42
Michael Lounello <mlounello@siena.edu>
2025-11-03 00:40:15 -0500

    Fix CORS for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,17 @@ import adminRoutes from "./routes/admin.js";
 const app = express();
 
 const allowedOrigin = env.CORS_ORIGIN || "http://localhost:5173";
+const allowedOrigins = process.env.CORS_ORIGIN.split(",");
 app.use(
   cors({
-    origin: allowedOrigin,
-    credentials: false,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 
